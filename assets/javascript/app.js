c6ab4e8d6e3af7ea3906ba69c5803ee8670bdd8c
smason27 <masonsteven27@gmail.com>
2019-04-11 23:49:28 -0700

    homework submission

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,71 @@
 $(document).ready(function () {
 
     var config = {
-        apiKey: "AIzaSyAlHWRQrMbKW_bTAu0W897tgciE0NEPs3Q",
-        authDomain: "train-times-c21f1.firebaseapp.com",
-        databaseURL: "https://train-times-c21f1.firebaseio.com",
-        projectId: "train-times-c21f1",
-        storageBucket: "train-times-c21f1.appspot.com",
-        messagingSenderId: "522739324977"
-    };
-    firebase.initializeApp(config);
+        apiKey: "AIzaSyDci3ME4-tlJwujNAvcmUMG5t_WABZySPE",
+        authDomain: "train-schedule-911f1.firebaseapp.com",
+        databaseURL: "https://train-schedule-911f1.firebaseio.com",
+        projectId: "train-schedule-911f1",
+        storageBucket: "",
+        messagingSenderId: "960806586004"
+      };
+      firebase.initializeApp(config)
 
     var database = firebase.database();
 
     var trainName = "";
     var destination = "";
-    var firstTrain = 0;
-    var frequency = "";
+    var firstTrain = "";
+    var frequency = 0;
+    var minutesAway = 0;
+    var trainArrival = 0;
 
-    $("#addTrain").on("click", function () {
-        event.preventDefault();
-
-        trainName = $("#trainName").val().trim();
-        destination = $("#destination").val().trim();
-        firstTrain = moment($("#firstTrain").val().trim(), "HH:mm").format("X");
-        frequency = $("#frequency").val().trim();
-
-        database.ref().push({
-            name: trainName,
-            destination: destination,
-            firstTrain: firstTrain,
-            frequency: frequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
-    });
     database.ref().on("child_added", function (snapshot) {
         var snap = snapshot.val();
-        console.log(snap)
-        console.log(moment() + "<--moment")
-        var firstTrainToday = moment.unix(firstTrain).format("HH:mm");
+        console.log(snap);
+        var firstTrainToday = moment(firstTrain, "HH:mm").subtract(1, "weeks");
         console.log(firstTrainToday)
-        var nextTrain = moment().diff(moment(firstTrainToday, moment()));
-        // var nextTrain = moment().diff(momemt(firstTrain, "minutes"));
+        var trainTimeDifference = moment().diff(moment(firstTrainToday), "minutes")
+        console.log(trainTimeDifference)
+        var trainDifference = trainTimeDifference % frequency;
+        console.log(trainDifference)
+        minutesAway = frequency - trainDifference;
+        console.log(minutesAway)
+        var nextTrain = moment().add(minutesAway, "mins");
         console.log(nextTrain)
-      
-        
+        trainArrival = moment(nextTrain).format("HH:mm");
+        console.log(trainArrival)
 
         $("#trainSchedule").find('tbody')
             .append($('<tr>')
                 .append($('<td>').text(snap.name))
                 .append($('<td>').text(snap.destination))
                 .append($('<td>').text(snap.frequency))
-                // .append($('<td>').text(nextTrain))
-                // .append($('<td>').text(minutesAway))
+                .append($('<td>').text(snap.trainArrival))
+                .append($('<td>').text(snap.minutesAway))
             )
     });
+   
+    $("#addTrain").on("click", function () {
+        event.preventDefault();
+
+        trainName = $("#trainName").val().trim();
+        console.log(trainName)
+        destination = $("#destination").val().trim();
+        console.log(destination)
+        firstTrain = $("#firstTrain").val().trim()
+        console.log(firstTrain)
+        frequency =parseInt($("#frequency").val().trim());
+        console.log(frequency)
+
+        database.ref().push({
+            name: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            minutesAway: minutesAway,
+            trainArrival: trainArrival,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+    });
+   
 });
